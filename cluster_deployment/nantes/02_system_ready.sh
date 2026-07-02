09ae19c772a9c850edb74261d7527ba1be166481
CKHuang-public <chihkaihuang1@gmail.com>
2026-07-02 23:36:13 +0200

    Update 02_system_ready.sh

--- cluster_deployment/nantes/02_system_ready.sh
+++ cluster_deployment/nantes/02_system_ready.sh
@@ -2,13 +2,13 @@
 
 manage=$(awk NR==1 cp_node_list)
 
-git clone https://github.com/CKHuangGH/dampscale
+git clone https://github.com/CKHuangGH/interclock
 rm -rf /home/chuang/.ssh/known_hosts
 
 for j in $(cat cp_node_list)
 do
     scp /home/chuang/.ssh/id_rsa root@$j:/root/.ssh
-    scp -r ./dampscale root@$j:/root/
+    scp -r ./interclock root@$j:/root/
 done
 
 echo "wait for 30 secs"
@@ -18,11 +18,11 @@ i=0
 for j in $(cat cp_node_list)
 do
 ssh -o StrictHostKeyChecking=no root@$j scp -o StrictHostKeyChecking=no /root/.kube/config root@$manage:/root/.kube/cluster$i
-ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/dampscale/
+ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/interclock/
 i=$((i+1))
 done
 
-scp cp_node_list root@$manage:/root/dampscale/package/cp_node_list
-scp all_node_list root@$manage:/root/dampscale/package/all_node_list
+scp cp_node_list root@$manage:/root/interclock/package/cp_node_list
+scp all_node_list root@$manage:/root/interclock/package/all_node_list
 
 echo "management cluster cp is $manage"
