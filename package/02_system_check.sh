5340557e26feac29d23788f0e7b217a3e3838203
CKHuang-public <chihkaihuang1@gmail.com>
2026-07-03 00:25:30 +0200

    Update 02_system_check.sh

--- package/02_system_check.sh
+++ package/02_system_check.sh
@@ -1,3 +1,10 @@
-kubectl get pod -A --field-selector=status.phase!=Running
+number=$(($(wc -l < cp_node_list) - 1))
+
+for i in `seq 0 $number`
+do
+	kubectl config use-context cluster$i
+	echo cluster$i
+	kubectl get pod -A --field-selector=status.phase!=Running
+done
 
 echo "screen -S mysession"
